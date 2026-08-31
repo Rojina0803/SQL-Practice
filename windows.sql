a1f6ec6df92485eb7927dc513c66f34aea2ac4b9
Rojina Dhakal <128691590+Rojina0803@users.noreply.github.com>
2026-08-31 19:57:05 +0545

    practice

--- windows.sql
+++ windows.sql
@@ -62,3 +62,27 @@ SELECT
 FROM employee_demographics dem
 JOIN employee_salary sal
     ON dem.employee_id = sal.employee_id;
+    
+    
+    WITH avg_salary AS (
+    SELECT
+        gender,
+        AVG(salary) AS avg_salary
+    FROM employee_demographics dem
+    JOIN employee_salary sal
+        ON dem.employee_id = sal.employee_id
+    GROUP BY gender
+)
+
+SELECT
+    dem.first_name,
+    dem.last_name,
+    dem.gender,
+    sal.salary,
+    avg_salary.avg_salary
+FROM employee_demographics dem
+JOIN employee_salary sal
+    ON dem.employee_id = sal.employee_id
+JOIN avg_salary
+    ON dem.gender = avg_salary.gender
+WHERE sal.salary > avg_salary.avg_salary;
